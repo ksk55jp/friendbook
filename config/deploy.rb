ef8a40940a94903f8384fef291db3d4043a6ad93
ksk55jp@gmail.com <ksk55jp@gmail.com>
2017-11-15 15:21:34 +0900

    Corrected Deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'friendbook'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/ksk55jp/friendbook'
@@ -10,7 +10,7 @@ set :repo_url, 'https://github.com/ksk55jp/friendbook'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/friendbook'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
